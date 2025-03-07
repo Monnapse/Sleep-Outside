ac05c13774a98580fda5c5d035d850fe6b1b0dea
Monnapse <masonshuber@gmail.com>
2025-03-07 10:57:32 -0700

    Fixed cart adding

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,21 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Check if item isnt already in the cart
+  const cartItems = getLocalStorage("so-cart") || [];
+  let found = false;
+  console.log(product);
+
+  cartItems.map((cartProduct, index) => {
+    if (cartProduct.Id == product.Id) found = true;
+  });
+
+  if (found === true) return;
+  console.log(product);
+  setLocalStorage("so-cart", [...cartItems, product]);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
